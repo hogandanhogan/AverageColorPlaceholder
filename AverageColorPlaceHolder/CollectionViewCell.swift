7966be4467386b1c45b183fb485926210ac5b330
Dan Hogan <hogandanhogan@gmail.com>
2015-07-13 13:22:29 -0400

    first commit

---
+++ AverageColorPlaceHolder/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  AverageColorPlaceHolder
+//
+//  Created by Dan Hogan on 7/12/15.
+//  Copyright (c) 2015 Dan Hogan. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
